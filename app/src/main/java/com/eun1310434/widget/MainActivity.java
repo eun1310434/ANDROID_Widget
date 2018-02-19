de1f1a966c8fa3617e0fec81b765126166c0acec
eun1310434 <eun1310434@naver.com>
2018-02-19 20:34:34 +0900

    Initial commit

---
+++ app/src/main/java/com/eun1310434/widget/MainActivity.java
@@ -0,0 +1,16 @@
+package com.eun1310434.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.button);
+        //setContentView(R.layout.edittext);
+        //setContentView(R.layout.image);
+    }
+}
